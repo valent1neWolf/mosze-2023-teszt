8a647ef6211fc62e63ce674b37255e1b30650a81
valent1neWolf <valentinewolf44v@gmail.com>
2023-09-16 14:37:31 +0200

    kikommentelem a hibákat amiket a kódban találtam

--- main.cpp
+++ main.cpp
@@ -4,23 +4,23 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS];    //hibás a változó neve
+    std::cout << '1-100 ertekek duplazasa'  //a sor nincs lezárva és hiányzik a ";"
+    for (int i = 0;)        //hiányosak a loop paraméterei
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++)     //itt is hiányosak a loop paraméterei
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:"       //nem irassa ki a kívánt értékeket és hiányzik a sor lezárása
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
     int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    for (int i = 0; i < N_ELEMENTS, i++)        //N_ELEMENTS után ";" kell nem pedig ","
     {
         atlag += b[i]
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
-    return 0;
+    return 0;       //hiányzik a dinamikusan lefoglalt memória felszabadítása
 }
